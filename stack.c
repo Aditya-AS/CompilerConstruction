ad23fe9da9851504d8d0220020f580637911a056
Abhinav <f2013183@pilani.bits-pilani.ac.in>
2016-03-04 00:32:12 +0530

    stack program

--- stack.c
+++ stack.c
@@ -1,15 +1,17 @@
 //program to make stack
 #include <stdio.h>
 #include <stdlib.h>
-#include "makeGrammar.c"
+#include "makeFollowSet.c"
 
 struct stack{
 	struct Node* head;
-}
+};
 
 struct stack* getStack(){
 	struct stack* newStack = (struct stack*)malloc(sizeof(struct stack));
-}
+	newStack->head = NULL;
+	return newStack;
+};
 
 void push(struct stack* st,char* name){
 	struct Node* element = getNode();
@@ -18,21 +20,21 @@ void push(struct stack* st,char* name){
 	element->next = st->head;
 	st->head = element;
 	return;
-} 
+}
 
 struct Node* pop(struct stack* st){
 	struct Node* element = getNode();
 	element->name = st->head->name;
-	element->is_token = checkToken(name);
-	element->nextNode = NULL;
-	st->head = st->head->name;
+	element->is_token = checkToken(st->head->name);
+	element->next = NULL;
+	st->head = st->head->next;
 	return element;
 }
 
 struct Node* top(struct stack* st){
 	struct Node* newNode = getNode();
-	newNode->name= st->head->name;
-	newNode->nextNode = NULL;
+	newNode->name = st->head->name;
+	newNode->next = NULL;
 	return newNode;
 }
 
