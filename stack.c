f1665484bcc7d952ccc7fa83705fa035f0b3de6b
Abhinav <f2013183@pilani.bits-pilani.ac.in>
2016-03-03 08:23:30 +0530

    stack program

---
+++ stack.c
@@ -0,0 +1,43 @@
+//program to make stack
+#include <stdio.h>
+#include <stdlib.h>
+#include "makeGrammar.c"
+
+struct stack{
+	struct Node* head;
+}
+
+struct stack* getStack(){
+	struct stack* newStack = (struct stack*)malloc(sizeof(struct stack));
+}
+
+void push(struct stack* st,char* name){
+	struct Node* element = getNode();
+	element->name = name;
+	element->is_token = checkToken(name);
+	element->next = st->head;
+	st->head = element;
+	return;
+} 
+
+struct Node* pop(struct stack* st){
+	struct Node* element = getNode();
+	element->name = st->head->name;
+	element->is_token = checkToken(name);
+	element->nextNode = NULL;
+	st->head = st->head->name;
+	return element;
+}
+
+struct Node* top(struct stack* st){
+	struct Node* newNode = getNode();
+	newNode->name= st->head->name;
+	newNode->nextNode = NULL;
+	return newNode;
+}
+
+
+bool checkEmpty(struct stack * st){
+	if(st->head==NULL) return true;
+	else return false;
+}
